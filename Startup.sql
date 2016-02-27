8f9cdcf59cdee08c4cf6eda716a9c0eab4f196cc
a4th8 <nwoodhea@mail.umw.edu>
2016-02-27 15:09:20 +0000

    Added the startup.sql file

--- Startup.sql
+++ Startup.sql
@@ -1,4 +1,27 @@
-DROP TABLE rooms;
+DROP DATABASE if EXISTS weddingtest;
+CREATE DATABASE weddingtest;
+\c weddingtest
+CREATE EXTENSION pgcrypto;
+CREATE TABLE users (username text NOT NULL PRIMARY KEY,
+                    password text NOT NULL);
+
+INSERT INTO users (username, password) VALUES ('4th8', crypt('0328940', gen_salt('bf')));
+                    
+CREATE TABLE messages (username text NOT NULL,
+                       message text NOT NULL default '',
+                       id SERIAL NOT NULL PRIMARY KEY);
+
+CREATE TABLE guests (id SERIAL PRIMARY KEY,
+                     first_name text NOT NULL,
+                     last_name text NOT NULL,
+                     rsvp VARCHAR(1) default 'F',
+                     number_of_extras integer default 1);
+
+CREATE TABLE gifts (name text PRIMARY KEY NOT NULL,
+                    price float8,
+                    link text,
+                    recieved VARCHAR(1) default 'F');
+
 CREATE TABLE rooms (id serial PRIMARY KEY NOT NULL,
                     roomname text NOT NULL,
                     username VARCHAR(25) NOT NULL references users(username));
